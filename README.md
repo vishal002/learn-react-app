173ae534dcf6a360879fbdfa3c9496c3c7199d6c
Vishal Kumar <vishal.kumar@qio.io>
2024-02-25 20:02:19 +0530

    props added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## App Snapsorts
 ![Home Page](https://github.com/vishal002/learn-react-app/blob/main/src/assets/snapsorts/home.jpg?raw=true)
+
+** Learning
+- Components
+- Props
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import reactImg from './assets/react-core-concepts.png'
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1))
+  return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
@@ -20,12 +22,50 @@ function Header() {
   );
 }
 
+function CoreConcept({ title, description, image }) {
+  return (
+    <li>
+      <img src={image} alt="" />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {/* <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            /> */}
+          </ul>
+        </section>
       </main>
     </div>
   );
